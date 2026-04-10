beda2dc091367d5ddbede8747bb112ace07830a5
GunSliger00007 <chaudharygehendra46@gmail.com>
2026-04-10 22:34:24 +0545

    added fix

--- src/api/locallevel-api.ts
+++ src/api/locallevel-api.ts
@@ -24,7 +24,7 @@ import { normalize } from '../utils/helpers.js';
  * @category LocalLevel
  */
 export function getLocalLevels(): LocalLevel[] {
-  return LOCAL_LEVELS;
+  return [...LOCAL_LEVELS];
 }
 
 /**
